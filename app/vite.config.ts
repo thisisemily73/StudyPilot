d61d9386f3a74a4ee139439b68ed56aefe5abb43
thisisemily73 <thisisemily73@gmail.com>
2026-08-18 15:02:22 +0000

    attempting deployment to recognize shared files

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: '/StudyPilot/app/',
 
   plugins: [react()],
-  
-  server: {
-    fs: {
-      allow: ['..'],
-    },
-  },
 
-  build: {
-    rollupOptions: {
-      input: './index.html',
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, '../shared'),
     },
   },
 })
