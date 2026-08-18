7542e076f278fb212508652a2af0099b0b879a0d
thisisemily73 <thisisemily73@gmail.com>
2026-08-18 14:33:39 +0000

    try again

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'app',
-
   base: '/StudyPilot/app/',
 
   plugins: [react()],
-
+  
   server: {
     fs: {
       allow: ['..'],
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: './index.html',
     },
   },
 })
