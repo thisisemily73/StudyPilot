dd3ca8121cca848b78efb235d3e0f32d1c60e7b5
thisisemily73 <thisisemily73@gmail.com>
2026-08-18 14:24:50 +0000

    tried fixing deployment error by editing vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: 'app',
+
   base: '/StudyPilot/app/',
+
   plugins: [react()],
+
   server: {
     fs: {
       allow: ['..'],
