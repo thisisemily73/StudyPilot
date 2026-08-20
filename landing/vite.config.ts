3bb564ee0350798526184125f7f983f37e1d90db
thisisemily73 <thisisemily73@gmail.com>
2026-08-20 04:02:25 +0000

    added paths to landing config

--- landing/vite.config.ts
+++ landing/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/StudyPilot/',
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@shared': path.resolve(__dirname, '../shared')
+    }
+  }
 })
