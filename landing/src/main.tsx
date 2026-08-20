bd61ef5ea243b5cb64dc6d3468a760277ec87448
thisisemily73 <thisisemily73@gmail.com>
2026-08-20 04:15:15 +0000

    changed path typo

--- landing/src/main.tsx
+++ landing/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import '@/shared/Variables.css'; // CHECK
+import '@shared/Variables.css'; // CHECK
 import './styles/Global.css';
 import './styles/Navbar.css';
 import './styles/Hero.css';
